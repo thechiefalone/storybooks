186e0cf382b8d856ba9573ee99a471f4b073988e
Alperen Özkan <alperenozkan@gmail.com>
2020-07-03 14:07:27 +0200

    not scripttags, striptags:)

--- app.js
+++ app.js
@@ -29,12 +29,16 @@ if (process.env.NODE_ENV === "development") {
 }
 
 //Handlebars Helpers
-const { formatDate } = require("./helpers/hbs");
+const { formatDate, stripTags, truncate } = require("./helpers/hbs");
 
 //Handlebars
 app.engine(
   ".hbs",
-  exphbs({ helpers: { formatDate }, defaultLayout: "main", extname: ".hbs" })
+  exphbs({
+    helpers: { formatDate, stripTags, truncate },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
 );
 app.set("view engine", ".hbs");
 
--- helpers/hbs.js
+++ helpers/hbs.js
@@ -6,15 +6,15 @@ module.exports = {
   },
   truncate: function (str, len) {
     if (str.length > len && str.length > 0) {
-      let new_str = str + "";
+      let new_str = str + " ";
       new_str = str.substr(0, len);
-      new_str = str.substr(0, new_str.lastIndexOf(""));
+      new_str = str.substr(0, new_str.lastIndexOf(" "));
       new_str = new_str.length > 0 ? new_str : str.substr(0, len);
       return new_str + "...";
     }
     return str;
   },
-  scriptTags: function (input) {
+  stripTags: function (input) {
     return input.replace(/<(?:.|\n)*?>/gm, "");
   },
 };
